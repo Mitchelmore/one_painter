55abc803d16d3db907169f538a870ae1f15041b8
Mitchelmore <45359937+Mitchelmore@users.noreply.github.com>
2023-04-09 22:25:51 +0200

    added updates from Abe404 / root_painter

--- painter/src/main/python/root_painter.py
+++ painter/src/main/python/root_painter.py
@@ -25,6 +25,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # too many public methods
 
 import sys
+import subprocess
 import os
 from pathlib import PurePath, Path
 import json
@@ -238,6 +239,8 @@ class OnePainter(QtWidgets.QMainWindow):
                                          self.val_annot_dir)
         self.update_image()
 
+        self.update_context_viewer()
+        
         self.scene.history = []
         self.scene.redo_list = []
 
@@ -246,6 +249,21 @@ class OnePainter(QtWidgets.QMainWindow):
 
         self.segment_current_image()
         self.update_window_title()
+
+
+    def update_context_viewer(self):
+        if hasattr(self, 'original_images'):
+            fname = os.path.splitext(self.png_fname)[0]
+            tile_num_str = fname.split('_')[-1]
+            fname = fname[:len(fname) - len('_' + tile_num_str)]
+
+            original_fname = None
+            for fname_with_ext in self.original_images:
+                if os.path.splitext(fname_with_ext)[0] == fname:
+                    original_fname = fname_with_ext
+                    break 
+            original_fpath = os.path.join(self.original_image_dir, original_fname)
+            self.context_viewer.update(original_fpath, self.graphics_view.image)
 
 
     def update_image(self):
@@ -450,6 +468,14 @@ class OnePainter(QtWidgets.QMainWindow):
         self.setWindowTitle("OnePainter")
         self.resize(layout.sizeHint())
 
+    def open_sync_directory(self):
+        if sys.platform == "darwin": # Mac/OSX
+            subprocess.call(['open', self.sync_dir])
+        elif sys.platform == 'win32': # Windows
+            os.startfile(self.sync_dir)
+        else: # assume linux/ubuntu
+            subprocess.call(['xdg-open', self.sync_dir])
+
     def specify_sync_directory(self):
         """ User may choose to update the sync directory.
             This may happen if they initially specified the wrong
@@ -499,6 +525,11 @@ class OnePainter(QtWidgets.QMainWindow):
         specify_sync_dir_btn.triggered.connect(self.specify_sync_directory)
         extras_menu.addAction(specify_sync_dir_btn)
 
+        open_sync_dir_btn = QtWidgets.QAction(QtGui.QIcon('missing.png'),
+                                                 'Open sync directory',
+                                                 self)
+        open_sync_dir_btn.triggered.connect(self.open_sync_directory)
+        extras_menu.addAction(open_sync_dir_btn)
 
         mask_btn = QtWidgets.QAction(QtGui.QIcon('missing.png'), 'Mask images', self)
         mask_btn.triggered.connect(self.show_mask_images)
@@ -849,13 +880,9 @@ class OnePainter(QtWidgets.QMainWindow):
         show_image_context_btn.setShortcut('Ctrl+C')
 
         def show_image_context():
-            fname = os.path.splitext(self.png_fname)[0]
-            tile_num_str = fname.split('_')[-1]
-            fname = fname[:len(fname) - len('_' + tile_num_str)]
             proj_settings = json.load(open(self.proj_file_path))
-
             if 'original_image_dir' in proj_settings:
-                original_image_dir = proj_settings['original_image_dir']
+                self.original_image_dir = proj_settings['original_image_dir']
             else:
                 # if the project doesn't yet have the path for the original
                 # images then ask the user for it.
@@ -864,28 +891,21 @@ class OnePainter(QtWidgets.QMainWindow):
                          "Please specify the original image directory.")
                 msg.setText(output)
                 msg.exec_()
-                original_image_dir = QtWidgets.QFileDialog.getExistingDirectory()
+                self.original_image_dir = QtWidgets.QFileDialog.getExistingDirectory()
                 if not original_image_dir:
                     return
                 else:
-                    proj_settings['original_image_dir'] = original_image_dir
+                    proj_settings['original_image_dir'] = self.original_image_dir
                     with open(self.proj_file_path, 'w') as file:
                         json.dump(proj_settings, file, indent=4)
 
-            original_images = os.listdir(original_image_dir)
-            original_fname = None
-            for fname_with_ext in original_images:
-                if os.path.splitext(fname_with_ext)[0] == fname:
-                    original_fname = fname_with_ext
-                    break 
-            original_fpath = os.path.join(original_image_dir, original_fname)
-            self.context_viewer = ContextViewer(original_fpath, self.graphics_view.image)
+            self.original_images = os.listdir(self.original_image_dir)
+            self.context_viewer = ContextViewer()
             self.context_viewer.show()
+            self.update_context_viewer()
 
         show_image_context_btn.triggered.connect(show_image_context)
-        print('view menu add action')
         view_menu.addAction(show_image_context_btn)
-
 
         def zoom_in():
             self.graphics_view.zoom *= 1.1
